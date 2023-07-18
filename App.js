18d3498c452ae72a21a6dea889df59dd9da3770c
nhunht <nhtn0311@gmail.com>
2023-07-18 16:46:30 +0700

    Upload payment for edit

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import Schedule from "./screens/Schedule";
 import NextSplash from "./screens/NextSplash";
 import SignUp from "./screens/SignUp";
 import SignIn from "./screens/SignIn";
+import PaymentMethod from "./screens/Payment";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -43,6 +44,7 @@ const HomeStack = () => {
       screenOptions={{ headerShown: true, headerTitleAlign: "center" }}
     >
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Payment" component={PaymentMethod} />
     </Stack.Navigator>
   )
 }
@@ -115,17 +117,22 @@ const BottomTab = () => {
 const App = () => {
   const [isFirstRun, setFirstRun] = useState(null);
   const [fontsLoaded, error] = useFonts({
+    "Nunito Sans_regular": require("./assets/fonts/Nunito_Sans_regular.ttf"),
+    "Nunito Sans_semibold": require("./assets/fonts/Nunito_Sans_semibold.ttf"),
+    "Nunito Sans_bold": require("./assets/fonts/Nunito_Sans_bold.ttf"),
+    "Nunito Sans_extrabold": require("./assets/fonts/Nunito_Sans_extrabold.ttf"),
+    Inter_regular: require("./assets/fonts/Inter_regular.ttf"),
+    Inter_medium: require("./assets/fonts/Inter_medium.ttf"),
+    Inter_semibold: require("./assets/fonts/Inter_semibold.ttf"),
+    "Roboto Mono_medium": require("./assets/fonts/Roboto_Mono_medium.ttf"),
     Poppins_regular: require("./assets/fonts/Poppins_regular.ttf"),
     Poppins_medium: require("./assets/fonts/Poppins_medium.ttf"),
     Poppins_bold: require("./assets/fonts/Poppins_bold.ttf"),
     Ubuntu_regular: require("./assets/fonts/Ubuntu_regular.ttf"),
     Ubuntu_bold: require("./assets/fonts/Ubuntu_bold.ttf"),
-    Inter_regular: require("./assets/fonts/Inter_regular.ttf"),
     Montserrat_medium: require("./assets/fonts/Montserrat_medium.ttf"),
     Montserrat_semibold: require("./assets/fonts/Montserrat_semibold.ttf"),
     Montserrat_bold: require("./assets/fonts/Montserrat_bold.ttf"),
-    "Nunito Sans_regular": require("./assets/fonts/Nunito_Sans_regular.ttf"),
-    "Nunito Sans_bold": require("./assets/fonts/Nunito_Sans_bold.ttf"),
     Nunito_regular: require("./assets/fonts/Nunito_regular.ttf"),
   });
 
